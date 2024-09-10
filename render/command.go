a86793187299514027c595553397752cfcb9de70
praxxley-tech <david.rennhard@gmail.com>
2024-09-10 15:41:05 +0200

    errorhandling whitestring

--- render/command.go
+++ render/command.go
@@ -195,7 +195,10 @@ func commandRenderer(
 			r.clear = false
 			r.finalized = true
 			output := termenv.NewOutput(os.Stdout)
-			output.WriteString(r.View())
+			if _, err := output.WriteString(r.View()); err != nil {
+				fmt.Fprintf(os.Stderr, "Fehler beim Schreiben des Outputs: %v\n", err)
+				return
+			}
 		}
 	}()
 	go func() {
