97545e24627bf02a67ab762b454663e78d9c48f9
praxxley-tech <david.rennhard@gmail.com>
2024-09-10 15:01:13 +0200

    splitted cmd commands

--- checks/command.go
+++ checks/command.go
@@ -14,19 +14,32 @@ func CLICommand(
 ) []api.CLICommandResult {
 	data := lesson.Lesson.LessonDataCLICommand.CLICommandData
 	responses := make([]api.CLICommandResult, len(data.Commands))
+
 	for i, command := range data.Commands {
 		finalCommand := interpolateArgs(command.Command, optionalPositionalArgs)
 		responses[i].FinalCommand = finalCommand
 
-		cmd := exec.Command(finalCommand)
+		parts := strings.Fields(finalCommand)
+		if len(parts) == 0 {
+			responses[i].ExitCode = -1
+			responses[i].Stdout = "Invalid command"
+			continue
+		}
+
+		cmd := exec.Command(parts[0], parts[1:]...)
+
 		b, err := cmd.Output()
 		if ee, ok := err.(*exec.ExitError); ok {
 			responses[i].ExitCode = ee.ExitCode()
 		} else if err != nil {
 			responses[i].ExitCode = -2
+		} else {
+			responses[i].ExitCode = 0
 		}
+
 		responses[i].Stdout = strings.TrimRight(string(b), " \n\t\r")
 	}
+
 	return responses
 }
 
