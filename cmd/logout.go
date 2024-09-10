e9505fe4dbc6a701f937e785e1f856b5cbde6c1d
praxxley-tech <david.rennhard@gmail.com>
2024-09-10 15:44:52 +0200

    error_handling

--- cmd/logout.go
+++ cmd/logout.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/spf13/cobra"
@@ -13,15 +14,35 @@ import (
 func logout() {
 	api_url := viper.GetString("api_url")
 	client := &http.Client{}
-	// Best effort - logout should never fail
-	r, _ := http.NewRequest("POST", api_url+"/v1/auth/logout", bytes.NewBuffer([]byte{}))
+
+	// Best effort - logout should never fail, but handle errors if they occur
+	r, err := http.NewRequest("POST", api_url+"/v1/auth/logout", bytes.NewBuffer([]byte{}))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fehler beim Erstellen der Logout-Anfrage: %v\n", err)
+		return
+	}
 	r.Header.Add("X-Refresh-Token", viper.GetString("refresh_token"))
-	client.Do(r)
+
+	resp, err := client.Do(r)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fehler beim Senden der Logout-Anfrage: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Fprintf(os.Stderr, "Logout-Anfrage fehlgeschlagen mit Statuscode: %d\n", resp.StatusCode)
+		return
+	}
 
 	viper.Set("access_token", "")
 	viper.Set("refresh_token", "")
 	viper.Set("last_refresh", time.Now().Unix())
-	viper.WriteConfig()
+	if err := viper.WriteConfig(); err != nil {
+		fmt.Fprintf(os.Stderr, "Fehler beim Schreiben der Konfigurationsdatei: %v\n", err)
+		return
+	}
+
 	fmt.Println("Logged out successfully.")
 }
 
